14ec399f37ce3da707e76df5e82c8755238909c8
Elena Capienko <elena.capienko@gmail.com>
2019-10-08 14:35:04 -0400

    init: add solution for test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+
+  let entered = expression.split('*');
+  let two = 0;
+  let five = 0;
+  
+  for (let step = 0; step < entered.length; step++) {
+    let theNumber;
+    let range;
+    
+    if (entered[step].endsWith('!!')) {
+      theNumber = entered[step].slice(0,-2);
+      range = 2;
+
+    } else {
+      theNumber = entered[step].slice(0,-1);
+      range = 1;
+    }
+
+    while (theNumber >= 1 ) {
+      let helper = theNumber;
+
+      while (helper % 5 == 0) {
+        helper = helper / 5;
+        five++;
+      }
+
+      helper = theNumber;
+      while (helper % 2 == 0) {
+        helper = helper / 2;
+        two++;
+      }
+
+      theNumber = theNumber - range;
+    }
+  }
+  return Math.min(two,five);
+}         // your solution
